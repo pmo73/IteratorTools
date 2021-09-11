e3e8b2bef21aa840f7b62f347491fdd5e9e98a03
Timmifixedit <tim@luchterhand.org>
2021-09-11 19:13:45 +0200

    init, working implementation

---
+++ Iterators.hpp
@@ -0,0 +1,157 @@
+//
+// Created by tim on 10.09.21.
+//
+
+#ifndef ITERATORTOOLS_ITERATORS_HPP
+#define ITERATORTOOLS_ITERATORS_HPP
+
+#include <tuple>
+
+namespace iterators {
+    template<bool Readonly = false, typename ...Iterable>
+    struct zip {
+        explicit zip(Iterable &...containers) : containers(containers...) {}
+        class ZipIterator {
+            using IteratorTuple = std::tuple<decltype(std::begin(
+                    std::declval<std::add_lvalue_reference_t<Iterable>>()))...>;
+            using ValueTuple = std::tuple<decltype(*std::begin(
+                    std::declval<std::add_lvalue_reference_t<Iterable>>()))...>;
+            using cValueTuple = std::tuple<decltype(*std::begin(
+                    std::declval<std::add_lvalue_reference_t<std::add_const_t<Iterable>>>()))...>;
+        public:
+            enum class Construct {
+                End
+            };
+
+            explicit ZipIterator(std::tuple<Iterable &...> &containers) :
+                    iterators(std::apply([](auto &&...c) { return std::tuple(std::begin(c)...); }, containers)),
+                    ends(std::apply([](auto &&...c) { return std::tuple(std::end(c)...); }, containers)) {
+            }
+
+            ZipIterator(std::tuple<Iterable &...> &containers, Construct) :
+                    iterators(std::apply([](auto &&...c) { return std::tuple(std::end(c)...); }, containers)),
+                    ends(iterators) {
+            }
+
+            ZipIterator &operator++() {
+                std::apply([] (auto &&...it) {(++it, ...);}, iterators);
+                return *this;
+            }
+
+            bool operator==(const ZipIterator &other) const {
+                return equal<0>(other.iterators);
+            }
+
+            bool operator!=(const ZipIterator &other) const {
+                return !(*this == other);
+            }
+
+            auto operator*() {
+                return dereference(RonlyDummy<Readonly>{});
+            }
+
+        private:
+            template<bool B>
+            struct RonlyDummy{};
+
+            auto dereference(RonlyDummy<false>) -> ValueTuple {
+                return std::apply([this] (auto &&...it) { return ValueTuple(*it...); }, iterators);
+            }
+
+            auto dereference(RonlyDummy<true>) -> cValueTuple {
+                return std::apply([this] (auto &&...it) { return ValueTuple(*it...); }, iterators);
+            }
+
+            template<std::size_t N>
+            [[nodiscard]] constexpr bool equal(const IteratorTuple &other, bool oneNotEqual = false) const {
+                if constexpr(N == std::tuple_size_v<IteratorTuple>) {
+                    return !oneNotEqual;
+                } else {
+                    bool itEqual = std::get<N>(iterators) == std::get<N>(other);
+                    if (itEqual && std::get<N>(iterators) == std::get<N>(ends)) {
+                        return true;
+                    }
+
+                    return equal<N + 1>(other, !itEqual || oneNotEqual);
+                }
+            }
+
+            IteratorTuple iterators;
+            IteratorTuple ends;
+        };
+
+        ZipIterator begin() {
+            return ZipIterator(containers);
+        }
+
+        ZipIterator end() {
+            return ZipIterator(containers, ZipIterator::Construct::End);
+        }
+
+    private:
+        std::tuple<Iterable &...> containers;
+    };
+
+    template<typename ...Iterable>
+    struct const_zip : public zip<true, Iterable...> {
+        explicit const_zip(Iterable &...iterable) : zip<true, Iterable...>(iterable...) {}
+    };
+
+    namespace impl {
+        template<typename T>
+        struct CounterIterator {
+            static_assert(std::is_integral_v<T> && ! std::is_floating_point_v<T>);
+            explicit constexpr CounterIterator(T begin, T end, T increment = T(1)) :
+                counter(begin), max(end), increment(increment) {}
+
+            CounterIterator &operator++() {
+                if (counter < max) {
+                    counter += increment;
+                }
+
+                return *this;
+            }
+
+            bool operator==(const CounterIterator &other) const {
+                return counter == other.counter;
+            }
+
+            bool operator!=(const CounterIterator &other) const {
+                return !(*this == other);
+            }
+
+            constexpr T operator*() const {
+                return counter;
+            }
+
+        private:
+            T counter;
+            T max;
+            T increment;
+        };
+
+        struct CounterContainer {
+            [[nodiscard]] static constexpr CounterIterator<std::size_t> begin() {
+                return CounterIterator<std::size_t>(0, std::numeric_limits<std::size_t>::max());
+            }
+
+            [[nodiscard]] static constexpr CounterIterator<std::size_t> end() {
+                return CounterIterator<std::size_t>(std::numeric_limits<std::size_t>::max(),
+                                                    std::numeric_limits<std::size_t>::max());
+            }
+        };
+    }
+
+    template<typename Container, bool Readonly = false>
+    struct enumerate : public zip<Readonly, impl::CounterContainer, Container> {
+        explicit enumerate(Container &c, impl::CounterContainer ctr = impl::CounterContainer{}) :
+            zip<Readonly, impl::CounterContainer, Container>(ctr, c) {}
+    };
+
+    template<typename Container>
+    struct const_enumerate : public enumerate<Container, true> {
+        explicit const_enumerate(Container &c) : enumerate<Container, true>(c) {}
+    };
+}
+
+#endif //ITERATORTOOLS_ITERATORS_HPP
