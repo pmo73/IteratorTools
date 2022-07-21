d08ff2fb082a15f630f7c5ec678ce31c2c9ddbe7
Timmifixedit <tim@luchterhand.org>
2022-07-21 12:50:34 +0200

    added further tests for CounterIterator

--- Test/CounterIterator.cpp
+++ Test/CounterIterator.cpp
@@ -32,3 +32,49 @@ TEST(CounterIterator, sentinel) {
     EXPECT_LT(test, Unreachable{});
     EXPECT_LE(test, Unreachable{});
 }
+
+TEST(CounterIterator, less_greater_difference_basic) {
+    using namespace iterators::impl;
+    EXPECT_LT(CounterIterator(17), CounterIterator(23));
+    EXPECT_LE(CounterIterator(17), CounterIterator(23));
+    EXPECT_LE(CounterIterator(17), CounterIterator(17));
+    EXPECT_GT(CounterIterator(17), CounterIterator(12));
+    EXPECT_GE(CounterIterator(17), CounterIterator(17));
+    EXPECT_EQ(CounterIterator(17) - CounterIterator(12), 5);
+    CounterIterator test(14, 2);
+    EXPECT_EQ(test - CounterIterator(2), 12);
+    EXPECT_EQ(CounterIterator(18) - test, 2);
+    EXPECT_EQ(test - CounterIterator(18), -4);
+    EXPECT_EQ(CounterIterator(10) - test, -2);
+}
+
+TEST(CounterIterator, negative_increment) {
+    using namespace iterators::impl;
+    CounterIterator reverse(0, -1);
+    EXPECT_EQ(*++reverse, -1);
+    EXPECT_EQ(*--reverse, 0);
+    EXPECT_EQ(reverse + 4, CounterIterator(-4));
+    EXPECT_EQ(reverse - 3, CounterIterator(3));
+    reverse += 3;
+    EXPECT_EQ(*reverse, -3);
+    EXPECT_EQ(reverse[7], -10);
+    reverse -= 2;
+    EXPECT_EQ(*reverse, -1);
+    EXPECT_EQ(reverse[-2], 1);
+
+}
+
+TEST(CounterIterator, less_greater_difference_reverse) {
+    using namespace iterators::impl;
+    CounterIterator reverseOne(4, -1);
+    EXPECT_EQ(reverseOne, CounterIterator(4, 3));
+    EXPECT_LT(reverseOne, CounterIterator(-1));
+    EXPECT_LE(reverseOne, CounterIterator(0));
+    EXPECT_GT(reverseOne, CounterIterator(6));
+    EXPECT_GE(reverseOne, CounterIterator(5));
+    CounterIterator reverseThree(7, -3);
+    EXPECT_EQ(reverseOne - reverseThree, 1);
+    EXPECT_EQ(reverseThree - reverseOne, -3);
+    reverseOne += 12;
+    EXPECT_EQ(reverseOne - reverseThree, 5);
+}
