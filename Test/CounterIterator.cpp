529b47ff96bf11f6bcc04d1bd5bb984d5ecb3e1c
Timmifixedit <tim@luchterhand.org>
2022-07-21 12:08:10 +0200

    added new test  for CounterIterator

---
+++ Test/CounterIterator.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "Iterators.hpp"
+
+TEST(CounterIterator, basic) {
+    using namespace iterators::impl;
+    CounterIterator test(0);
+    EXPECT_EQ(*test, 0);
+    EXPECT_EQ(*++test, 1);
+    EXPECT_EQ(*test++, 1);
+    EXPECT_EQ(*test, 2);
+    EXPECT_EQ(test, CounterIterator(2));
+    EXPECT_NE(test, CounterIterator(3));
+    EXPECT_EQ(--test, CounterIterator(1));
+    EXPECT_EQ(test--, CounterIterator(1));
+    EXPECT_EQ(*test, 0);
+    EXPECT_EQ(test + 3, CounterIterator(3));
+    EXPECT_EQ(3 + test, CounterIterator(3));
+    test += 5;
+    EXPECT_EQ(*test, 5);
+    EXPECT_EQ(test - 2, CounterIterator(3));
+    test -= 3;
+    EXPECT_EQ(*test, 2);
+    EXPECT_EQ(test[4], 6);
+}
